ad74936973ff2f8c2574788c49e969def5c2b366
SummitKwan <SummitKwan@hotmail.com>
2016-09-12 17:33:06 -0400

    Upload folder TDT2PLX2TDT
    matlab functions for Sheinberg lab neuro data processing pipline: concatenate TDT blocks for offline spike sorting using Plexon Offline Sorter.  It offers several functions:
    1) Select and convert data recorded with TDT digital signal processor, concatenate blocks together and convert to a plx file
    2) After spike sorting is done, convert the sort code back to TDT blocks
    3) Convert the TDT blocks to matlab format
    3) upload datafiles to the lab server

---
+++ TDT2PLX2TDT/TDT2PLX2TDT_script.m
@@ -0,0 +1,15 @@
+cd('L:/tdt/export_mat/TDT2PLX2TDT');
+set_default_data_path;
+addpath(genpath('Matlab Offline Files SDK'))
+
+%% TDT2PLX
+TDT2PLX('', {}, 'PLXDIR', DEFAULT_PLX_PATH);
+
+%% PLX2TDT
+PLX2TDT('');
+
+%% convert to matlab format and upload to shared disk
+dataman_TDT('dat');
+
+%% Get computer name
+getenv('COMPUTERNAME')
