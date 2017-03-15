093ad0a2b6903342455200588f707244dcc8d685
SummitKwan <SummitKwan@hotmail.com>
2017-03-15 12:49:17 -0400

    add necessary annotations to code

--- perceptron_demo/perceptron.py
+++ perceptron_demo/perceptron.py
@@ -1,5 +1,6 @@
 """
-Demo for CS2951m presentation: perceptron algorithm for large margin classifier
+Demo for Brown CS2951m presentation: Perceptron algorithm for large margin classifier
+Shaobo Guan, Shaobo_Guan@brown.edu
 """
 # run ./perceptron_demo/perceptron
 
@@ -7,21 +8,34 @@ import numpy as np
 from matplotlib import pyplot as plt
 plt.style.use('ggplot')
 
+""" funcitons for the parametric online perceptron algorithm """
 
 def evaluate_x(x, v):
+    """
+    evalute the perceptron output
+    :param x: data point, (n) vector or (m*n) array;  where n is the dim of data and m is the number of data points
+    :param v: the weight parameter of perceptron. (n+1) vector, where v[0] is the bias term
+    :return: binary assignment of x
+    """
     if len(x.shape)==2:
         return np.sign(np.sum(x*v[1:], axis=1)+v[0])
     elif len(x.shape) == 1:
         return np.sign(np.sum(x * v[1:]) + v[0])
 
+
 def voted_evaluate_x(x, V, C):
+    """ for voted perceptron """
     return np.sign(np.sum(np.array([evaluate_x(x,v)*c for v,c in zip(V,C)]), axis=0))
 
+
 def ini_v():
+    """ generate a random data boundary """
     theta_org = np.random.rand(1) * np.pi * 2
     return np.array([(np.random.rand(1)[0] - 0.5)*0.8, np.cos(theta_org), np.sin(theta_org)])
 
+
 def draw_boundary(v, grid_space = np.arange(-1,1,0.001), tf_plot=True):
+    """ draw decision bounday of percetrpon with parameter v """
     grid_space_x, grid_space_y = np.meshgrid(grid_space, grid_space)
     grid_assign = np.sign(v[0] + grid_space_x*v[1] + grid_space_y*v[2])
     if tf_plot:
@@ -30,7 +44,9 @@ def draw_boundary(v, grid_space = np.arange(-1,1,0.001), tf_plot=True):
                  levels=[-1.5,0,1.5], alpha=0.1, cmap='bwr')
     return grid_assign
 
+
 def draw_voted_boundary(V,C, grid_space = np.arange(-1,1,0.001), tf_plot_multiple_levels=False):
+    """ draw decision bounday of voted percetrpon with parameter V and C """
     grid_space_x, grid_space_y = np.meshgrid(grid_space, grid_space)
     grid_assign = grid_space_x*0
     for v,c in zip(V,C):
@@ -52,9 +68,23 @@ def perceptron_update(v, x, y):
     return v
 
 
+""" funcitons for the non-parametric online perceptron algorithm (kernel perceptron) """
+
+
 def kernel_evaluate_x(x, data_keep, d=1, sigma=None):
+    """
+    evalute the kernel perceptron output, can choose between polunomial kernel of degree d or RBF kernel of scale sigma,
+    if sigma is None, use polynomial kernel, if sigma is a number, use RBF kernel.
+    by default use polynomial kernel of degree one (identical the origian perceptron)
+    :param x:         data to evaluate
+    :param data_keep: the valuable data (those that the perceptron misclassified in the past) stored by the algorithm
+    :param d:         degree of polynomial kernel
+    :param sigma:     scale of the RBF kernel
+    :return:          +1 of -1
+    """
     xs = data_keep[0]
     ys = data_keep[1]
+    """ calculate similarity (defined by the kernel) between the new data point with every every valuable data """
     if sigma is None:  # polynomial kernel
         y_hat = np.sign( np.sum( ( np.sum(xs* np.expand_dims(x, 0), axis=1) +1)**d *ys) )
     else:              # rbf kernel
@@ -63,6 +93,10 @@ def kernel_evaluate_x(x, data_keep, d=1, sigma=None):
 
 
 def kernel_perceptron_update(data_keep, x, y, d=1, sigma=None):
+    """
+    update the kernel perceptron: if misclassified, append the x, y to data_keep,
+    which stores all the valuable data points and their labels
+    """
     xs = data_keep[0]
     ys = data_keep[1]
     if kernel_evaluate_x(x, data_keep, d, sigma) * y <=0:
@@ -72,7 +106,7 @@ def kernel_perceptron_update(data_keep, x, y, d=1, sigma=None):
 
 
 def kernel_draw_boundary(data_keep, grid_space = np.arange(-1,1,0.01), tf_plot=True, d=1, sigma=None):
-
+    """ draw decision bounday of kernel perceptron """
     grid_space_x, grid_space_y = np.meshgrid(grid_space, grid_space)
     N_grid = len(grid_space_x)
     x_ravel = np.zeros([N_grid**2, 2])
@@ -89,23 +123,59 @@ def kernel_draw_boundary(data_keep, grid_space = np.arange(-1,1,0.01), tf_plot=T
     return grid_assign
 
 
-""" original online perceptron """
+
+
+""" ====================  online perceptron  ==================== """
+
+""" <<<<<<<<<< parameters to choose for users, start ---------- """
+data_boundary = 'linear'     # the true boundary of data, choose from ['linear', 'curved', 'tai-chi']
+
+T = 1                        # number of iterations over the dataset
+type_perceptron = 'parametric'   # choose from ['parametic', 'kernel']
+d = 5                        # degree of polynomial kernel
+sigma = 0.2                  # scale of RBF kernel, if None, use polynomial kernel, otherwise if a number, use rbf kernel
+tf_plot = True               # if plot
+tf_savefig = False           # if save figures
+i_plot_interval = 1          # the interval of plotting, eg., if set to 10, plot once after going over every 10 data points
+path_savefig =  './perceptron_demo/temp_figs'   # path to save figures
+
+if type_perceptron == 'kernel':
+    print('use the kernel perceptron')
+    if sigma is None:
+        print('kernel: polynomial kernel with degree {}'.format(d))
+    else:
+        print('kernel: RBF kernel with scale {}'.format(sigma))
+else:
+    print('use the origianl parametric perceptron')
+
+""" ---------- parameters to choose for users, end   >>>>>>>>>> """
+
+
+""" generate data  """
 np.random.seed(seed=1)
 
-m = 500   # number of data points
-dim = 2  # number of dimensions
-R = 1    # largest radius of X
+m = 50   # number of data points
+dim = 2   # number of dimensions
+R = 1     # largest radius of X
 
 
 # generate X in Range
 X = (np.random.rand(m*10, dim)*2-1)*R
 X = X[np.sum(X**2, axis=1)<=R, :][:m, :]
 
-# generate Y
-v_org = np.array([0.2, 0.6, 0.8])
-Y = evaluate_x(X, v_org)
+# assigne labels to x
 
-if True:
+if data_boundary == 'linear':
+    v_org = np.array([0.2, 0.6, 0.8])
+    Y = evaluate_x(X, v_org)
+elif data_boundary == 'curved':
+    x1=X[:,0]
+    x2=X[:,1]
+    Y[x1>0]= 1
+    Y[x1<0]=-1
+    Y[x1**2+(x2+0.5)**2<=0.5**2]= 1
+    Y[x1**2+(x2-0.5)**2<=0.5**2]=-1
+elif data_boundary == 'tai-chi':
     x1=X[:,0]
     x2=X[:,1]
     Y[x1>0]= 1
@@ -114,31 +184,33 @@ if True:
     Y[x1**2+(x2-0.5)**2<=0.5**2]=-1
     Y[x1**2+(x2+0.5)**2<=0.15**2]=-1
     Y[x1**2+(x2-0.5)**2<=0.15**2]=+1
+else:
+    print('parameter data_boundary not recognized, must be one of "linear", "curved" or "tai-chi" ')
 
 
+""" plot the origianl data """
 plt.plot(X[Y>0,0], X[Y>0,1], 'rP')
 plt.plot(X[Y<0,0], X[Y<0,1], 'bo')
-draw_boundary(v_org)
 plt.axis('square')
 plt.xlim([-1,1])
 plt.ylim([-1,1])
+plt.title('all data points to be classified')
+print('the original data')
+plt.pause(1.0)
 
-v=np.zeros(3)
-data_keep = [np.zeros([1,2]), np.zeros(1)]
-T = 5
-type_perceptron = 'kernel'   # 'linear', 'kernel'
-d = 5
-sigma = 0.4
-tf_plot = True
-tf_savefig = True
-i_plot_interval = m
 
+""" initial parameters for the online perceptron """
+v=np.zeros(3)                        # for the orignal parametric perceptron
+data_keep = [X[0:1,:], Y[0:1]]       # for the kernel perceptron
+
+print('perceptron online updating starts')
 for t in range(T):
     N_error=0
     for i in range(m):
         x = X[i, :]
         y = Y[i]
 
+        """ plot before update """
         if tf_plot and i%i_plot_interval == 0:
             plt.cla()
             if t==0:
@@ -158,12 +230,12 @@ for t in range(T):
             plt.title('t={}/{}, n={}/{}, N_error={}/{}_before'.format(t + 1, T, i + 1, m, N_error, i + 1))
 
             if tf_savefig:
-                plt.savefig('./perceptron_demo/temp_figs/perceptron_T{}_i{}_before.png'.format(t, i))
+                plt.savefig('{}/perceptron_T{}_i{}_before.png'.format(path_savefig, t, i))
             else:
                 plt.show()
                 plt.pause(0.1)
 
-
+        """ perceptron update step """
         if type_perceptron == 'kernel':
             data_keep_new = kernel_perceptron_update(data_keep, x, y, d=d, sigma=sigma)
             if len(data_keep_new[1]) != len(data_keep[1]):
@@ -175,6 +247,7 @@ for t in range(T):
                 N_error=N_error+1
             v=v_new
 
+        """ plot after update """
         if tf_plot and i%i_plot_interval == 0:
 
             plt.cla()
@@ -195,56 +268,9 @@ for t in range(T):
             plt.title('t={}/{}, n={}/{}, N_error={}/{}_update'.format(t + 1, T, i + 1, m, N_error, i+1))
 
             if tf_savefig:
-                plt.savefig('./perceptron_demo/temp_figs/perceptron_T{}_i{}_update.png'.format(t, i))
+                plt.savefig('{}/perceptron_T{}_i{}_update.png'.format(path_savefig, t, i))
             else:
                 plt.show()
                 plt.pause(0.1)
 
 
-
-""" voted perceptron """
-v=np.zeros(3)
-T = 10
-tf_plot = True
-tf_savefig = True
-V = [v]
-C = [0]
-for t in range(T):
-    N_error=0
-    for i in range(m):
-
-        x=X[i,:]
-        y=Y[i]
-        v_new = perceptron_update(v, x, y)
-        if np.any(v_new != v):
-            N_error=N_error+1
-            V.append(v_new)
-            C.append(1)
-        else:
-            C[-1] += 1
-        v=v_new
-
-        if tf_plot:
-            plt.cla()
-            plt.scatter(X[:,0], X[:,1], c=Y, cmap='bwr', vmin=-1, vmax=1,edgecolors='w')
-            plt.scatter(x[0], x[1], c='g', s=100, marker='X', alpha=0.5)
-
-            draw_boundary(v)
-
-            plt.axis('square')
-            plt.xlim([-1, 1])
-            plt.ylim([-1, 1])
-            plt.title('t={}/{}, n={}/{}, N_error={}/{}'.format(t + 1, T, i + 1, m, N_error, i+1))
-
-            if tf_savefig:
-                plt.savefig('./perceptron_demo/temp_figs/perceptron_T{}_i{}.png'.format(t, i))
-            else:
-                plt.show()
-                plt.pause(0.1)
-
-plt.scatter(X[:, 0], X[:, 1], c=Y, cmap='bwr', vmin=-1, vmax=1, edgecolors='w')
-plt.scatter(x[0], x[1], c='g', s=100, marker='X', alpha=0.5)
-draw_voted_boundary(V,C, tf_plot_multiple_levels=True)
-plt.axis('square')
-plt.xlim([-1, 1])
-plt.ylim([-1, 1])
